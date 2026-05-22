e1b36bbe0ea42b3371382ea92c73a2ec64cf546a
Caio Pedro <caiopedrovianadasilva@gmail.com>
2026-05-22 15:17:26 -0300

    feat: implement authentication service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+
+export class AuthService {
+
+  private http = inject(HttpClient);
+
+  private api = 'http/localhost:8080/auth';
+
+  register(data:any) {
+    return this.http.post(`${this.api}/register`, data);
+  }
+
+  login(data:any) {
+    return this.http.post<any>(`${this.api}/login`, data);
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  isAuthenticated() {
+    return !!this.getToken();
+  }
+
+}
