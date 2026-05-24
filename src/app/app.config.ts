eba175ed2af8cd5a69ec3d03e1604d247fedd61c
Caio Pedro <caiopedrovianadasilva@gmail.com>
2026-05-24 18:23:12 -0300

    chore: enable forms support in Angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './interceptors/jwt-interceptor';
-
+import { provideZoneChangeDetection } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({
+      eventCoalescing: true
+    }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([jwtInterceptor]))
+    provideHttpClient(withInterceptors([jwtInterceptor])),
+    importProvidersFrom(FormsModule)
   ]
 };
